0b3ebbffb3c4e3bfcf745429a78f6d44c54fed3b
Supun Budhajeewa <budhajeewa@thesanmark.com>
2018-08-09 14:49:13 +0530

    Initial commit.
    Create project based on https://github.com/sanmark/illuminated-phinx.

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# Sanmark Janus : DB Structure
+
+This repository holds public DB structure release for [Sanmark Janus](https://github.com/sanmark/janus-system).
---
+++ tmp.config.php
@@ -0,0 +1,7 @@
+<?php
+
+define ( 'DB_HOST' , '127.0.0.1' ) ;
+define ( 'DB_NAME' , 'db_name' ) ;
+define ( 'DB_USER' , 'root' ) ;
+define ( 'DB_PASSWORD' , 'root' ) ;
+define ( 'DB_PORT' , 3306 ) ;
